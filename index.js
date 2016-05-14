3a705b6a7d13c3acd7298d44d43d08c419127c4c
Ryan X. Charles <ryanxcharles@gmail.com>
2016-05-14 12:57:29 +0200

    use nonenumerable properties for inject & injected

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  * ========
  *
  * This is a dependency injector specifically designed for use with Yours
- * Bitcoin.  In order to allow injecting dependencies, Yours Bitcoin classes
+ * Bitcoin. In order to allow injecting dependencies, Yours Bitcoin classes
  * provide an "inject" method that makes a new class with injected
- * dependencies.  However, that method by itself presents a problem, because it
+ * dependencies. However, that method by itself presents a problem, because it
  * creates a new class every time it is used, leaving the burden of caching the
  * classes to the user. Thus the injector allows us to easily wrap an inject
  * method that keeps a cache of the created classes, lowing memory burden, and
@@ -28,8 +28,17 @@ module.exports = function injector (inject, dependencies) {
 
     Class = inject(Object.assign({}, dependencies, deps))
     classmap.set(deps, Class)
-    Class.inject = meminject
-    Class.injected = deps
+
+    // The "inject" and "injected" properties are non-enumerable so they don't
+    // ruin any code that may enumerate properties of your classes.
+    Object.defineProperty(Class, 'inject', {
+      value: meminject,
+      enumerable: false
+    })
+    Object.defineProperty(Class, 'injected', {
+      value: deps,
+      enumerable: false
+    })
     return Class
   }
 
